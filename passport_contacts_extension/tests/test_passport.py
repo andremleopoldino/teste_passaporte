f60e291583c8a31f2ea6e20c4f8daa7aae190274
André Moreira Leopoldino <101368597+andremleopoldino@users.noreply.github.com>
2023-07-31 16:08:45 -0300

    Update test_passport.py

--- passport_contacts_extension/tests/test_passport.py
+++ passport_contacts_extension/tests/test_passport.py
@@ -7,12 +7,12 @@ class ContatosExtensaoTestCase(TransactionCase):
             model_test = self.env['res.partner']
             model_test_data = {
                 'name': 'André Leopoldino',
-                'nome_completo': 'André Leopoldino',
+                'name_document_holder': 'André Leopoldino',
                 'passport_number': 'AB1234567',
 
             }
             partner = model_test.create(model_test_data)
-            if partner.numero_do_passaporte and not re.match(r'^[A-Z]{2}\d{7}$', partner.numero_do_passaporte):
+            if partner.passport_number and not re.match(r'^[A-Z]{2}\d{7}$', partner.passport_number):
                 print('Formato invalido')
             else:
                 print('Formato valido')
